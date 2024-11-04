e7556607909618da735636dfee725ae5b91e9468
Kurapati Avinash <159903300+aadhiavi@users.noreply.github.com>
2024-11-04 12:45:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const contactRoutes = require("./routes/contact");
 const connectRoutes = require("./routes/connect");
 const visitRoutes = require("./routes/visit");
+const chatRoutes = require('./routes/chat');
 require('dotenv').config();
 require("./conn/conn");
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use("/api/v1", contactRoutes);
 app.use('/api/v1', connectRoutes);
 app.use('/api/v1', visitRoutes);
+app.use('/api/v1/', chatRoutes);
 
 app.listen(5000, () => {
     console.log("Server started at port 5000");
